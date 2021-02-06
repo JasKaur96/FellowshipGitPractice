e9c42d46838ecb05af3d8887dda04512c6f49a36
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-06 10:24:57 +0530

    Added: New SOP is added

--- helloWorldProg.java
+++ helloWorldProg.java
@@ -1,6 +1,8 @@
 class helloWorldProg{
 public static void main( String[] args )
-    {  System.out.println("Hello World!!!!");
-
+{	
+	System.out.println("Hello World!!!!");  
+	System.out.println("UC1 code here.");
+	
 }
 }
