209362115256e69247be8195cd4f356884fe3f48
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-07 21:36:21 +0530

    Refactered: A print statement.

--- helloWorldProg.java
+++ helloWorldProg.java
@@ -2,7 +2,7 @@ class helloWorldProg{
 public static void main( String[] args )
 {	
 	System.out.println("Hello World!!!!");  
-	System.out.println("UC1 code here.");
+	System.out.println("UC1 code here for conflict.");
 	
 }
 }
